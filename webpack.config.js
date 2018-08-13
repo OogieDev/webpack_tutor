65aec1159eb74c44be5717104702aea606fe856c
Вадим <oogie.webdev@gmail.com>
2018-08-13 12:59:44 +0300

    lesson15commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,37 @@
 const path = require('path');
+const extractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
 
     context: path.resolve(__dirname, 'src'),
 
-    entry: './init.js',
+    entry: {
+        index:'./index'
+    },
 
     output: {
-        filename: 'bundle.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist')
     },
+
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: extractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use:  ['css-loader', 'less-loader']
+                })
+            }
+        ]
+    },
+
+    plugins: [
+        new extractTextPlugin({
+            filename: 'styles.css',
+            allChunks: true
+        })
+    ]
 
 
  };
