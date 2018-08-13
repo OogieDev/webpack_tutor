935db3d140abbabbcb055839ebaa77cc371ec760
Вадим <oogie.webdev@gmail.com>
2018-08-13 12:22:09 +0300

    webpack.Css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,36 @@
 const path = require('path');
+const extractTextWebpackPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
 
     context: path.resolve(__dirname, 'src'),
 
-    entry: './init.js',
+    entry: {
+        index: './index',
+        styles: './styles.css'
+    },
 
     output: {
-        filename: 'bundle.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist')
     },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                //use: ['style-loader', 'css-loader']
+                use: extractTextWebpackPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
+                })
+            }
+        ]
+    },
+
+    plugins: [
+        new extractTextWebpackPlugin('[name].css')
+    ]
 
 
  };
